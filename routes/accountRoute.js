e851ddd955d7496d18a69933644d812be6c81a66
Jester572 <jester572@yahoo.com>
2023-11-30 18:53:24 -0700

    Added server-side data validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,15 +1,21 @@
 // Needed Resources 
 const express = require("express")
-const router = new express.Router() 
+const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require('../utilities/index');
 const Util = require("../utilities/index");
+const regValidate = require('../utilities/account-validation')
 
 router.get("/login", Util.handleErrors(accountController.buildLogin))
 
-router.get("/register",Util.handleErrors(accountController.buildRegister))
+router.get("/register", Util.handleErrors(accountController.buildRegister))
 
-router.post('/register', Util.handleErrors(accountController.registerAccount))
+router.post(
+    '/register',
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    Util.handleErrors(accountController.registerAccount)
+)
 
 
 module.exports = router
